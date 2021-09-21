546b6e5703ad97bddd05145f22e1b64ae8709b01
madsnyborg <1087348@ucn.dk>
2021-09-21 23:34:15 +0200

    messing around in tool

--- toolbox-BE/models/tool.js
+++ toolbox-BE/models/tool.js
@@ -221,18 +221,20 @@ class Tool {
                 // › › close DB connection
 
                 try {
-                    this.authors.forEach(async (author) => {
-                        const authorCheck = await Author.readById(author.authorid);
+                    this.tools.forEach(async (tool) => {
+                        const toolCheck = await Tool.readById(tool.toolid);
                     });
 
                     const pool = await sql.connect(con);
                     const resultCheckTool = await pool.request()
-                        .input('title', sql.NVarChar(50), this.title)
-                        .input('year', sql.Int(), this.year)
+                        .input('toolTitle', sql.NVarChar(50), this.toolTitle)
+                        .input('toolDescription', sql.NVarChar(50), this.toolDescription)
+                        .input('toolLink', sql.NVarChar(255), this.toolLink)
+
                         .query(`
                             SELECT *
-                            FROM liloTool b
-                            WHERE b.title = @title AND b.year = @year
+                            FROM tool t
+                            WHERE t.toolTitle = @toolTitle AND @toolDescription = @toolDescription AND t.toolLink = @toolLink
                         `)
 
                     if (resultCheckTool.recordset.length == 1) throw { statusCode: 409, errorMessage: `Conflict. Tool already exists, toolid: ${resultCheckTool.recordset[0].toolid}` }
@@ -240,34 +242,23 @@ class Tool {
 
                     await pool.connect();
                     const result00 = await pool.request()
-                        .input('title', sql.NVarChar(50), this.title)
-                        .input('year', sql.Int(), this.year)
-                        .input('link', sql.NVarChar(255), this.link)
-                        .input('authorid', sql.Int(), this.authors[0].authorid)
+                        .input('toolTitle', sql.NVarChar(50), this.toolTitle)
+                        .input('toolDescription', sql.NVarChar(50), this.toolDescription)
+                        .input('toolLink', sql.NVarChar(255), this.toolLink)
                         .query(`
-                                INSERT INTO liloTool (title, year, link)
-                                VALUES (@title, @year, @link);
-                        
-                                SELECT *
-                                FROM liloTool
-                                WHERE toolid = SCOPE_IDENTITY();
-
-                                INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
-                                VALUES (SCOPE_IDENTITY(), @authorid);
+                               
                         `)
 
                     if (!result00.recordset[0]) throw { statusCode: 500, errorMessage: `DB server error, INSERT failed.` }
                     const toolid = result00.recordset[0].toolid;
 
-                    this.authors.forEach(async (author, index) => {
+                    this.tools.forEach(async (tool, index) => {
                         if (index > 0) {
                             await pool.connect();
-                            const resultAuthors = await pool.request()
+                            const resultTools = await pool.request()
                                 .input('toolid', sql.Int(), toolid)
-                                .input('authorid', sql.Int(), author.authorid)
                                 .query(`
-                                    INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
-                                    VALUES (@toolid, @authorid)
+                                
                                 `)
                         }
                     })
@@ -283,6 +274,70 @@ class Tool {
                 }
 
                 sql.close();
+
+                // try {
+                //     this.authors.forEach(async (author) => {
+                //         const authorCheck = await Author.readById(author.authorid);
+                //     });
+
+                //     const pool = await sql.connect(con);
+                //     const resultCheckTool = await pool.request()
+                //         .input('title', sql.NVarChar(50), this.title)
+                //         .input('year', sql.Int(), this.year)
+                //         .query(`
+                //             SELECT *
+                //             FROM liloTool b
+                //             WHERE b.title = @title AND b.year = @year
+                //         `)
+
+                //     if (resultCheckTool.recordset.length == 1) throw { statusCode: 409, errorMessage: `Conflict. Tool already exists, toolid: ${resultCheckTool.recordset[0].toolid}` }
+                //     if (resultCheckTool.recordset.length > 1) throw { statusCode: 500, errorMessage: `Multiple hits of unique data. Corrupt database, toolid: ${resultCheckTool.recordset[0].toolid}` }
+
+                //     await pool.connect();
+                //     const result00 = await pool.request()
+                //         .input('title', sql.NVarChar(50), this.title)
+                //         .input('year', sql.Int(), this.year)
+                //         .input('link', sql.NVarChar(255), this.link)
+                //         .input('authorid', sql.Int(), this.authors[0].authorid)
+                //         .query(`
+                //                 INSERT INTO liloTool (title, year, link)
+                //                 VALUES (@title, @year, @link);
+                        
+                //                 SELECT *
+                //                 FROM liloTool
+                //                 WHERE toolid = SCOPE_IDENTITY();
+
+                //                 INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
+                //                 VALUES (SCOPE_IDENTITY(), @authorid);
+                //         `)
+
+                //     if (!result00.recordset[0]) throw { statusCode: 500, errorMessage: `DB server error, INSERT failed.` }
+                //     const toolid = result00.recordset[0].toolid;
+
+                //     this.authors.forEach(async (author, index) => {
+                //         if (index > 0) {
+                //             await pool.connect();
+                //             const resultAuthors = await pool.request()
+                //                 .input('toolid', sql.Int(), toolid)
+                //                 .input('authorid', sql.Int(), author.authorid)
+                //                 .query(`
+                //                     INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
+                //                     VALUES (@toolid, @authorid)
+                //                 `)
+                //         }
+                //     })
+
+                //     sql.close();
+
+                //     const tool = await Tool.readById(toolid);
+
+                //     resolve(tool);
+
+                // } catch (error) {
+                //     reject(error);
+                // }
+
+                // sql.close();
 
             })();
         });
@@ -354,7 +409,7 @@ class Tool {
                         .input('year', sql.Int(), this.year)
                         .input('link', sql.NVarChar(255), this.link)
                         .input('toolid', sql.Int(), this.toolid)
-                        .input('authorid', sql.Int(), this.authors[0].authorid)
+                  //    .input('authorid', sql.Int(), this.authors[0].authorid)
                         .query(`
                             UPDATE Tool
                             SET
@@ -379,16 +434,20 @@ class Tool {
                         //     VALUES (@toolid, @authorid)
                         // `);
 
-                    this.authors.forEach(async (author, index) => {
+                  //  this.authors.forEach(async (author, index) => {
+                    this.tools.forEach(async (tool, index) => {
                         if (index > 0) {
                             await pool.connect();
-                            const resultAuthors = await pool.request()
+                            const resultTools = await pool.request()
                                 .input('toolid', sql.Int(), this.toolid)
-                                .input('authorid', sql.Int(), author.authorid)
+                            //  .input('authorid', sql.Int(), author.authorid)
                                 .query(`
-                                        INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
-                                        VALUES (@toolid, @authorid)
+                                        
                                     `);
+                                // .query(`
+                                //         INSERT INTO liloToolAuthor (FK_toolid, FK_authorid)
+                                //         VALUES (@toolid, @authorid)
+                                //     `);
                         }
                     });
 
